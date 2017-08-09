6e815b8ed360aa277f5c6695b2935d3c7bca73e8
Dee Hoi Tú <30512431+BetaSchrodinger@users.noreply.github.com>
2017-08-09 02:46:32 -0700

    Update mymap.cpp

--- src/mymap.cpp
+++ src/mymap.cpp
@@ -1,229 +1,75 @@
+
+#include "encoding.h"
 #include "mymap.h"
-#include "vector.h"
-using namespace std;
+#include "pqueue.h"
 
-MyMap::MyMap() {
-    nBuckets = 257; //ASCII including the extended ones, have total of 255 characters
-    buckets = createBucketArray(nBuckets);
-    nElems = 0;
-}
 
-MyMap::~MyMap() {
-    for (int i=0; i < nBuckets; i++) {
-        if (buckets[i] != nullptr) {
-            delete buckets[i];
-            buckets[i] = nullptr;
+MyMap buildFrequencyTable(istream& input) {
+    MyMap freqTable;
+    int character = input.get();
+    while (!input.eof()) {
+        //read the file til the end
+        if (!freqTable.containsKey(character)) {
+            //if the character is not in the map, put it in the map and set the count to 1
+            freqTable.put(character, 1);
+            character = input.get();
+        }else {
+            //if the charcter is in the map already, take out the count and plus 1 and put it back
+            int count = freqTable.get(character);
+            count += 1;
+            freqTable.put(character, count);
+            character = input.get();
         }
     }
+    //now we have to deal with EOF
+    freqTable.put(256, 1);
+    return freqTable;
 }
 
-void MyMap::put(int key, int value) {
-    key_val_pair* newK = new key_val_pair;
-    newK->key = key;
-    newK->value = value;
-    newK->next = nullptr;
-    int temp = hashFunction(key) % nBuckets;
-    if (buckets[temp] != nullptr) {
-        buckets[temp]->value = value;
+HuffmanNode* buildEncodingTree(const MyMap& freqTable) {
+    PriorityQueue<HuffmanNode*> freqList;
+    Vector<int>keys = freqTable.keys();
+    for (int key:keys) {
+        HuffmanNode *charNode = new HuffmanNode;
+        charNode->character = key;
+        charNode->count = freqTable.get(key);
+        freqList.enqueue(charNode, charNode->count);
     }
-    else {
-        buckets[temp] = newK;
+    //now we have a priorityqueue with nodes(storing the char) sorted from the least frequent to the most frequent
+    while (!(freqList.size() == 1 && freqList.peek()->character == NOT_A_CHAR)) {
+        //if the queue has not only the root
+        HuffmanNode* curr = new HuffmanNode;
+        curr->character = NOT_A_CHAR;
+        curr->zero = freqList.dequeue();
+        if (freqList.size() > 0) {curr->one = freqList.dequeue();}
+        curr->count = curr->zero->count + curr->one->count;
+        freqList.enqueue(curr, curr->count);
     }
-    nElems ++;
+    return freqList.dequeue();
 }
 
-int MyMap::get(int key) const {
-    int temp = hashFunction(key) % nBuckets;
-    if (buckets[temp] == nullptr) throw ("The key is not existed in the map yet!");
-    else return buckets[temp]->value;
+Map<int, string> buildEncodingMap(HuffmanNode* encodingTree) {
+    // TODO: implement this function
+    Map<int, string> encodingMap;   // this is just a placeholder so it will compile
+    return encodingMap;             // this is just a placeholder so it will compile
 }
 
-bool MyMap::containsKey(int key) {
-    int temp = hashFunction(key) % nBuckets;
-    if (buckets[temp] != nullptr) return true;
-    else return false;
+void encodeData(istream& input, const Map<int, string>& encodingMap, obitstream& output) {
+    // TODO: implement this function
 }
 
-Vector<int> MyMap::keys() const {
-     Vector<int> keys;
-     for (int i = 0; i < nBuckets; i++ ) {
-         if (buckets[i] != nullptr) {
-             keys.add(buckets[i]->key);
-         }
-     }
-     return keys;
+void decodeData(ibitstream& input, HuffmanNode* encodingTree, ostream& output) {
+    // TODO: implement this function
 }
 
-int MyMap::size() {
-    return nElems;
+void compress(istream& input, obitstream& output) {
+    // TODO: implement this function
 }
 
-/**
- * STARTER CODE, DO NOT MODIFY
- */
-
-// copy constructor
-MyMap::MyMap(const MyMap &myMap) {
-    // make a deep copy of the map
-    nBuckets = myMap.nBuckets;
-
-    buckets = createBucketArray(nBuckets);
-
-    // walk through the old array and add all elements to this one
-    Vector<int> keys = myMap.keys();
-    for (int i=0; i < keys.size(); i++) {
-        int key = keys[i];
-        int value = myMap.get(key);
-        put(key,value);
-    }
-
+void decompress(ibitstream& input, ostream& output) {
+    // TODO: implement this function
 }
 
-// assignment overload
-MyMap& MyMap::operator= (const MyMap &myMap) {
-    // make a deep copy of the map
-
-    // watch for self-assignment
-    if (this == &myMap) {
-        return *this;
-    }
-
-    // if data exists in the map, delete it
-    for (int i=0; i < nBuckets; i++) {
-        MyMap::key_val_pair* bucket = buckets[i];
-        while (bucket != nullptr) {
-            // walk the linked list and delete each node
-            MyMap::key_val_pair* temp = bucket;
-            bucket = bucket->next;
-            delete temp;
-        }
-        buckets[i] = nullptr;
-    }
-    nElems = 0;
-    // walk through the old array and add all elements to this one
-    Vector<int> keys = myMap.keys();
-    for (int i=0; i < keys.size(); i++) {
-        int key = keys[i];
-        int value = myMap.get(key);
-        put(key,value);
-    }
-
-    // return the existing object so we can chain this operator
-    return *this;
-}
-
-ostream &operator<<(ostream &out, MyMap &myMap) {
-    out << "{";
-    Vector<int> keys = myMap.keys();
-    for (int i=0; i < keys.size(); i++) {
-        int key = keys[i];
-        int value = myMap.get(key);
-        out << key << ":" << value;
-        if (i < keys.size() - 1) { // print the comma after each pair except the last
-            out << ", ";
-        }
-    }
-    out << "}";
-    return out;
-}
-
-istream &operator>>(istream &in, MyMap &myMap) {
-    // assume the format {1:2, 3:4}
-    bool done = false;
-    in.get(); // get the first char, {
-    int nextChar = in.get(); // get the first real character
-    while (!done) {
-        string nextInput;
-        while (nextChar != ',' and nextChar != '}') {
-                nextInput += nextChar;
-                nextChar = in.get();
-        }
-        if (nextChar == ',') {
-            // read the space as well
-            in.get(); // should be a space
-            nextChar = in.get(); // get the next character
-        } else {
-            done = true; // we have reached }
-        }
-        // at this point, nextInput should be in the form 1:2
-        // (we should have two integers separated by a colon)
-        // BUT, we might have an empty map (special case)
-        if (nextInput != "") {
-            vector<string> kvp = stringSplit(nextInput,":");
-            myMap.put(stringToInteger(kvp[0]),stringToInteger(kvp[1]));
-        }
-    }
-    return in;
-}
-/**
- * STARTER CODE, DO NOT MODIFY
- *
- * Given a number of buckets, creates a hashtable (array of linked list heads).
- *
- * @param nBuckets the number of buckets you want in the hashtable.
- * return an array of heads of linked lists of key_val_pairs
- */
-MyMap::bucketArray MyMap::createBucketArray(int nBuckets) {
-    bucketArray newBuckets = new key_val_pair*[nBuckets];
-    for (int i = 0; i < nBuckets; i++) {
-        newBuckets[i] = nullptr;
-    }
-    return newBuckets;
-}
-
-/**
- * STARTER CODE
- * The hash function for your HashMap implementation.
- * For an extension, you might want to improve this function.
- *
- * @param input - an integer to be hashed
- * return the hashed integer
- */
-int MyMap::hashFunction(int input) const {
-    // use unsigned integers for calculation
-    // we are also using so-called "magic numbers"
-    // see https://stackoverflow.com/a/12996028/561677 for details
-    unsigned int temp = ((input >> 16) ^ input) * 0x45d9f3b;
-    temp = ((temp >> 16) ^ temp) * 0x45d9f3b;
-    temp = (temp >> 16) ^ temp;
-
-    // convert back to positive signed int
-    // (note: this ignores half the possible hashes!)
-    int hash = (int) temp;
-    if (hash < 0) {
-        hash *= -1;
-    }
-
-    return hash;
-}
-
-/**
-* @brief MyMap::sanityCheck
-*
-* Checks a map for internal consistency
-*/
-void MyMap::sanityCheck(){
-  for(int i = 0; i < 1000; i++) {
-      put(i,i);
-  }
-  for(int i = 0; i < 1000; i++) {
-
-      if(!containsKey(i)) {
-          string err = integerToString(i) + " should be a key in the map but cannot be found";
-      }
-      int val;
-
-      try {
-          val = get(i);
-      } catch(string exception) {
-          string err = "Unable to get value for " + integerToString(i);
-          throw(err);
-      }
-
-      if (i != val) {
-          string err = integerToString(i) + " should be mapped to " + integerToString(i) + " but is mapped to " + integerToString(val) + "instead";
-          throw(err);
-      }
-  }
-  cout << "Map seems ok" << endl;
+void freeTree(HuffmanNode* node) {
+    // TODO: implement this function
 }
